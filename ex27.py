f178b0119b96f6dc6ef5667854bab2955e900121
Evan Slaton <evanslaton@gmail.com>
2020-01-15 11:02:00 -0800

    Finished exercises 27-30

---
+++ ex27.py
@@ -0,0 +1,9 @@
+print(not(True or False))
+print(not(True or True))
+print(not(False or True))
+print(not(False or False))
+
+print(not(True and False))
+print(not(True and True))
+print(not(False and True))
+print(not(False and False))
